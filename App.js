378781955b233b1ff24b470d8bfb52f218b8324e
unknown <naninarendra2000@gmail.com>
2024-11-13 22:44:40 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,3 @@
+const parente=React.createElement("div",{id:"parente"},[React.createElement("div",{id:"chelid"},React.createElement("h1",{id:"parente"},"hellonani")),React.createElement("div",{id:"chelid"},React.createElement("h1",{id:"parente"},"hellonani"))]);
+const root =ReactDOM.createRoot(document.getElementById("root"));
+root.render(parente)
